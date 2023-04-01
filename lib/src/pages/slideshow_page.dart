71f40e49a9bc23cb71f8ae3f62b8e76536e5650c
Cristóbal Rodríguez <cristobalrodbar@gmail.com>
2023-04-01 14:39:00 -0600

    sección 5

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -1,23 +1,39 @@
 import 'package:custom_painter/src/widgets/slideshow.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:flutter_svg/svg.dart';
 
 class SlideshowPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        //backgroundColor: Colors.grey,
-        body: Slideshow(
-      puntosArriba: true,
-      colorPrimario: Colors.red,
-      colorSecundario: Colors.grey,
-      slides: [
+        // backgroundColor: Colors.purple,
+        // body: MiSlideshow(),
+        body: Column(
+      children: <Widget>[
+        Expanded(child: MiSlideshow()),
+        Expanded(child: MiSlideshow())
+      ],
+    ));
+  }
+}
+
+class MiSlideshow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Slideshow(
+      bulletPrimario: 20,
+      bulletSecundario: 12,
+      // puntosArriba: true,
+      colorPrimario: Color(0xffFF5A7E),
+      // colorSecundario: Colors.red,
+      slides: <Widget>[
         SvgPicture.asset('assets/svgs/slide-1.svg'),
         SvgPicture.asset('assets/svgs/slide-2.svg'),
         SvgPicture.asset('assets/svgs/slide-3.svg'),
         SvgPicture.asset('assets/svgs/slide-4.svg'),
-        SvgPicture.asset('assets/svgs/slide-5.svg')
+        SvgPicture.asset('assets/svgs/slide-5.svg'),
       ],
-    ));
+    );
   }
 }
--- lib/src/widgets/slideshow.dart
+++ lib/src/widgets/slideshow.dart
@@ -1,119 +1,145 @@
-import 'package:custom_painter/src/models/slider_model.dart';
 import 'package:flutter/material.dart';
-//borrar
-//import 'package:flutter_svg/svg.dart';
+
 import 'package:provider/provider.dart';
 
 class Slideshow extends StatelessWidget {
   final List<Widget> slides;
   final bool puntosArriba;
-  final Color colorPrimario, colorSecundario;
+  final Color colorPrimario;
+  final Color colorSecundario;
+  final double bulletPrimario;
+  final double bulletSecundario;
 
-  const Slideshow(
-      {required this.slides,
-      this.puntosArriba = false,
-      this.colorPrimario = Colors.blue,
-      this.colorSecundario = Colors.grey});
+  Slideshow({
+    required this.slides,
+    this.puntosArriba = false,
+    this.colorPrimario = Colors.blue,
+    this.colorSecundario = Colors.grey,
+    this.bulletPrimario = 12.0,
+    this.bulletSecundario = 12.0,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => SliderModel(),
+      create: (_) => new _SlideshowModel(),
       child: SafeArea(
-        child: Center(
-            child: Column(
-          children: [
-            if (this.puntosArriba)
-              _Dots(
-                  this.slides.length, this.colorPrimario, this.colorSecundario),
-            Expanded(child: _Slides(this.slides)),
-            if (!this.puntosArriba)
-              _Dots(
-                  this.slides.length, this.colorPrimario, this.colorSecundario),
-          ],
+        child: Center(child: Builder(
+          builder: (BuildContext context) {
+            Provider.of<_SlideshowModel>(context).colorPrimario =
+                this.colorPrimario;
+            Provider.of<_SlideshowModel>(context).colorSecundario =
+                this.colorSecundario;
+
+            Provider.of<_SlideshowModel>(context).bulletPrimario =
+                this.bulletPrimario;
+            Provider.of<_SlideshowModel>(context).bulletSecundario =
+                this.bulletSecundario;
+
+            return _CrearEstructuraSlideshow(
+                puntosArriba: puntosArriba, slides: slides);
+          },
         )),
       ),
     );
   }
 }
 
+class _CrearEstructuraSlideshow extends StatelessWidget {
+  const _CrearEstructuraSlideshow({
+    Key? key,
+    required this.puntosArriba,
+    required this.slides,
+  }) : super(key: key);
+
+  final bool puntosArriba;
+  final List<Widget> slides;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        if (this.puntosArriba) _Dots(this.slides.length),
+        Expanded(child: _Slides(this.slides)),
+        if (!this.puntosArriba) _Dots(this.slides.length),
+      ],
+    );
+  }
+}
+
 class _Dots extends StatelessWidget {
   final int totalSlides;
-  final Color colorPrimario, colorSecundario;
-  _Dots(this.totalSlides, this.colorPrimario, this.colorSecundario);
+
+  _Dots(this.totalSlides);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: 70,
-      //color: Colors.red,
       child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: /*  [_Dot(0), _Dot(1), _Dot(2)] */
-              List.generate(
-                  this.totalSlides,
-                  (index) =>
-                      _Dot(index, this.colorPrimario, this.colorSecundario))
-          /*  this
-                .slides
-                .map((slide) => _Dot(this.slides.indexOf(slide)))
-                .toList(), */
-          ),
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(this.totalSlides, (i) => _Dot(i)),
+      ),
     );
   }
 }
 
 class _Dot extends StatelessWidget {
   final int index;
-  final Color colorPrimario, colorSecundario;
 
-  _Dot(this.index, this.colorPrimario, this.colorSecundario);
+  _Dot(this.index);
 
   @override
   Widget build(BuildContext context) {
-    final pageViewIndex = Provider.of<SliderModel>(context).currentPage;
+    final ssModel = Provider.of<_SlideshowModel>(context);
+    double tamano;
+    Color color;
+
+    if (ssModel.currentPage >= index - 0.5 &&
+        ssModel.currentPage < index + 0.5) {
+      tamano = ssModel.bulletPrimario;
+      color = ssModel.colorPrimario;
+    } else {
+      tamano = ssModel.bulletSecundario;
+      color = ssModel.colorSecundario;
+    }
 
     return AnimatedContainer(
-      margin: EdgeInsets.symmetric(horizontal: 5),
-      width: 12,
-      height: 12,
-      decoration: BoxDecoration(
-          color: (pageViewIndex >= index - 0.5 && pageViewIndex < index + 0.5)
-              ? this.colorPrimario
-              : this.colorSecundario,
-          shape: BoxShape.circle),
       duration: Duration(milliseconds: 200),
+      width: tamano,
+      height: tamano,
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(color: color, shape: BoxShape.circle),
     );
   }
 }
 
-// SvgPicture.asset('assets/svgs/slide-1.svg')
 class _Slides extends StatefulWidget {
   final List<Widget> slides;
+
   _Slides(this.slides);
 
   @override
-  State<_Slides> createState() => _SlidesState();
+  __SlidesState createState() => __SlidesState();
 }
 
-class _SlidesState extends State<_Slides> {
-  final pageViewController = PageController();
+class __SlidesState extends State<_Slides> {
+  final pageViewController = new PageController();
 
   @override
   void initState() {
+    super.initState();
+
     pageViewController.addListener(() {
-      Provider.of<SliderModel>(context, listen: false).currentPage =
+      Provider.of<_SlideshowModel>(context, listen: false).currentPage =
           pageViewController.page!;
     });
-    // TODO: implement initState
-    super.initState();
   }
 
   @override
   void dispose() {
     pageViewController.dispose();
-    // TODO: implement dispose
     super.dispose();
   }
 
@@ -122,30 +148,63 @@ class _SlidesState extends State<_Slides> {
     return Container(
       child: PageView(
         controller: pageViewController,
+        // children: <Widget>[
+        //   _Slide( 'assets/svgs/slide-1.svg' ),
+        //   _Slide( 'assets/svgs/slide-2.svg' ),
+        //   _Slide( 'assets/svgs/slide-3.svg' ),
+        // ],
         children: widget.slides.map((slide) => _Slide(slide)).toList(),
-        /*children: <Widget>[
-          const _Slide('assets/svgs/slide-1.svg'),
-          const _Slide('assets/svgs/slide-2.svg'),
-          const _Slide('assets/svgs/slide-3.svg') 
-        ],*/
       ),
     );
   }
 }
 
 class _Slide extends StatelessWidget {
-  final Widget svg;
+  final Widget slide;
 
-  const _Slide(this.svg);
+  _Slide(this.slide);
 
   @override
   Widget build(BuildContext context) {
     return Container(
         width: double.infinity,
         height: double.infinity,
-        padding: const EdgeInsets.all(30),
-        child: svg
-        //SvgPicture.asset(svg),
-        );
+        padding: EdgeInsets.all(30),
+        child: slide);
+  }
+}
+
+class _SlideshowModel with ChangeNotifier {
+  double _currentPage = 0;
+  Color _colorPrimario = Colors.blue;
+  Color _colorSecundario = Colors.grey;
+  double _bulletPrimario = 12;
+  double _bulletSecundario = 12;
+
+  double get currentPage => this._currentPage;
+
+  set currentPage(double pagina) {
+    this._currentPage = pagina;
+    notifyListeners();
+  }
+
+  Color get colorPrimario => this._colorPrimario;
+  set colorPrimario(Color color) {
+    this._colorPrimario = color;
+  }
+
+  Color get colorSecundario => this._colorSecundario;
+  set colorSecundario(Color color) {
+    this._colorSecundario = color;
+  }
+
+  double get bulletPrimario => this._bulletPrimario;
+  set bulletPrimario(double tamano) {
+    this._bulletPrimario = tamano;
+  }
+
+  double get bulletSecundario => this._bulletSecundario;
+  set bulletSecundario(double tamano) {
+    this._bulletSecundario = tamano;
   }
 }
