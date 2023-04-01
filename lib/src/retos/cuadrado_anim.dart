27ad7d549f37d72cef753732bd7214faf4a1a643
Cristóbal Rodríguez <cristobalrodbar@gmail.com>
2023-03-31 19:14:52 -0600

    cuadrado animado

--- lib/src/retos/cuadrado_anim.dart
+++ lib/src/retos/cuadrado_anim.dart
@@ -16,12 +16,43 @@ class _CuadradoAnimado extends StatefulWidget {
   State<_CuadradoAnimado> createState() => _CuadradoAnimadoState();
 }
 
-class _CuadradoAnimadoState extends State<_CuadradoAnimado> {
+class _CuadradoAnimadoState extends State<_CuadradoAnimado>
+    with SingleTickerProviderStateMixin {
   late AnimationController controller;
+  late Animation<double> moverDerecha, moverArriba, moverIzquierda, moverAbajo;
 
   @override
   void initState() {
     // TODO: implement initState
+    controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 4500));
+
+    moverDerecha = Tween(begin: 0.0, end: 100.0).animate(CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.0, 0.25, curve: Curves.bounceOut)));
+
+    moverArriba = Tween(begin: 0.0, end: -100.0).animate(CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.25, 0.50, curve: Curves.bounceOut)));
+
+    moverIzquierda = Tween(begin: 0.0, end: -100.0).animate(CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.50, 0.75, curve: Curves.bounceOut)));
+
+    moverAbajo = Tween(begin: 0.0, end: 100.0).animate(CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.75, 1.0, curve: Curves.bounceOut)));
+
+    controller.addListener(() {
+      //print('status ' + controller.status.toString());
+      if (controller.status == AnimationStatus.completed) {
+        //controller.reverse();
+        controller.reset();
+      } /* else if (controller.status == AnimationStatus.dismissed) {
+        controller.forward();
+      } */
+    });
+
     super.initState();
   }
 
@@ -34,7 +65,17 @@ class _CuadradoAnimadoState extends State<_CuadradoAnimado> {
 
   @override
   Widget build(BuildContext context) {
-    return _Rectangulo();
+    controller.forward();
+    return AnimatedBuilder(
+        child: _Rectangulo(),
+        animation: controller,
+        builder: (context, child) {
+          return Transform.translate(
+            offset: Offset(moverDerecha.value + moverIzquierda.value,
+                moverArriba.value + moverAbajo.value),
+            child: child,
+          );
+        });
   }
 }
 
